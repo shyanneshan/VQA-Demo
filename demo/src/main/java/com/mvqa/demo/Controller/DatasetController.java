56244f68e5624f2ce00fb452d1607f7b3c22b67b
Shyanne Shan <60920622+shyanneshan@users.noreply.github.com>
2021-07-10 13:18:31 +0800

    Update DatasetController.java

--- demo/src/main/java/com/mvqa/demo/Controller/DatasetController.java
+++ demo/src/main/java/com/mvqa/demo/Controller/DatasetController.java
@@ -2,9 +2,14 @@ package com.mvqa.demo.Controller;
 
 import com.mvqa.demo.Service.DatasetService;
 import com.mvqa.demo.model.po.DatasetPo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.*;
 import java.util.ArrayList;
 
 @RestController
@@ -12,7 +17,7 @@ import java.util.ArrayList;
 public class DatasetController {
     @Autowired
     private DatasetService datasetService;
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatasetController.class);
     @GetMapping("/dataSets")
     public ArrayList<DatasetPo> getDatasets(){
         return datasetService.getAllDatasets();
@@ -47,4 +52,50 @@ public class DatasetController {
             datasetPo.setValid(valid);
         datasetService.addDataset(datasetPo);
         }
+    @PostMapping("/upload")
+    @ResponseBody
+    @CrossOrigin
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath = "/home/wxl/Documents/VQADEMO/demo/src/main/resources/datasetOriginalData/";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            LOGGER.info("上传成功");
+            python();
+            return "上传成功";
+        } catch (IOException e) {
+            LOGGER.error(e.toString(), e);
+        }
+        return "上传失败！";
+    }
+    public static void python() throws IOException {
+        String env="python";
+        File dic=new File(".");
+        System.out.println(dic.getCanonicalFile());
+        String model=dic.getCanonicalFile()+"/src/main/resources/Medical-named-entity-recognition-master/Medical-named-entity-recognition-master/Medical-named-entity-recognition-master/main.py";
+        String  cmd=env+" "+model;
+        System.out.println(cmd);
+        Runtime run=Runtime.getRuntime();
+        try{
+            Process process=run.exec(cmd);
+            InputStream in=process.getInputStream();
+            InputStreamReader reader=new InputStreamReader(in);
+            BufferedReader br =new BufferedReader(reader);
+            StringBuffer sb=new StringBuffer();
+            String message;
+            while((message=br.readLine())!=null){
+                sb.append(message);
+            }
+            System.out.println(sb);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
